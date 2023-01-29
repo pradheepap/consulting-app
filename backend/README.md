e86a46e330b065d45cec125579481d7fc5d1474c
pradheepap <pradheepa.p@gmail.com>
2023-01-29 17:15:21 +0800

    Insert Mongo Records

--- backend/README.md
+++ backend/README.md
@@ -9,6 +9,12 @@ GET : http://localhost:5050/services
 ## TODO
 
 - Update Services Collection {Post}
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"email":"xyz@xyz.com","id":"123"}' \
+  http://localhost:5050/services/
+
 - Delete Services Collection {Post}
 
 
--- backend/routes/records.js
+++ backend/routes/records.js
@@ -18,37 +18,39 @@ recordRoutes.route('/services').get(async function (_req, res) {
     .limit(50)
     .toArray(function (err, result) {
       if (err) {
+        console.log('Error fetching Service listings!');
         res.status(400).send('Error fetching Service listings!');
       } else {
+        console.log('fetching Service listings!', result);
         res.json(result);
       }
     });
 });
 
 // This section will help you create a new record.
-// recordRoutes.route('/listings/recordSwipe').post(function (req, res) {
-//   const dbConnect = dbo.getDb();
-//   const matchDocument = {
-//     listing_id: req.body.id,
-//     last_modified: new Date(),
-//     session_id: req.body.session_id,
-//     direction: req.body.direction,
-//   };
+recordRoutes.route('/services/').post(function (req, res) {
+  
+  const dbConnect = dbo.getDb();
+  const matchDocument = {
+    email: req.body.email,
+    id: req.body.id,
+  };
 
-//   dbConnect
-//     .collection('matches')
-//     .insertOne(matchDocument, function (err, result) {
-//       if (err) {
-//         res.status(400).send('Error inserting matches!');
-//       } else {
-//         console.log(`Added a new match with id ${result.insertedId}`);
-//         res.status(204).send();
-//       }
-//     });
-// });
+  dbConnect
+    .collection('service_records')
+    .insertOne(matchDocument, function (err, result) {
+      if (err) {
+        console.log("Error..", res)
+        res.status(400).send('Error inserting matches!');
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+});
 
 // // This section will help you update a record by id.
-// recordRoutes.route('/listings/updateLike').post(function (req, res) {
+// recordRoutes.route('/services/:id').post(function (req, res) {
 //   const dbConnect = dbo.getDb();
 //   const listingQuery = { _id: req.body.id };
 //   const updates = {
@@ -71,21 +73,21 @@ recordRoutes.route('/services').get(async function (_req, res) {
 // });
 
 // // This section will help you delete a record.
-// recordRoutes.route('/listings/delete/:id').delete((req, res) => {
-//   const dbConnect = dbo.getDb();
-//   const listingQuery = { listing_id: req.body.id };
+recordRoutes.route('/services/delete/:id').delete((req, res) => {
+  const dbConnect = dbo.getDb();
+  const servicesQuery = { services_id: req.body.id };
 
-//   dbConnect
-//     .collection('listingsAndReviews')
-//     .deleteOne(listingQuery, function (err, _result) {
-//       if (err) {
-//         res
-//           .status(400)
-//           .send(`Error deleting listing with id ${listingQuery.listing_id}!`);
-//       } else {
-//         console.log('1 document deleted');
-//       }
-//     });
-//});
+  dbConnect
+    .collection('service_records')
+    .deleteOne(servicesQuery, function (err, _result) {
+      if (err) {
+        res
+          .status(400)
+          .send(`Error deleting listing with id ${servicesQuery.services_id}!`);
+      } else {
+        console.log('1 document deleted');
+      }
+    });
+});
 
 module.exports = recordRoutes;
