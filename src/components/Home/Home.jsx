374982b9b6b3a8affc40a299c9359c4cf1c59542
unknown <meiltokalicharan@gmail.com>
2023-02-05 21:24:36 +0530

    build tools component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header'
+import BuildTools from '../BuildTools/BuildTools'
 
 const Home = () => {
     return (
         <main>
             <Header/>
+            <BuildTools/>
         </main>
 
     );
