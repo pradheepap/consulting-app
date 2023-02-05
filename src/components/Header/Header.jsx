1acd874a4a31bf27b4c3371b0bbc0a6d1635add2
pradheepap <pradheepa.p@gmail.com>
2023-02-05 16:13:15 +0800

    Nav component created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css'
 import Hero from '../Hero/Hero';
+import NavBar from '../NavBar/NavBar'
 
 const Header = () => {
     return (
         <section className="header">
+            <NavBar/>
             <Hero/>
         </section>
     );
