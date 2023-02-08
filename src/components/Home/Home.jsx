a4d2a0e538c51e8c0196cbde9092e13be8c24c74
unknown <meiltokalicharan@gmail.com>
2023-02-08 23:07:07 +0530

    fix review component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from '../Header/Header'
-import BuildTools from '../BuildTools/BuildTools'
-import About from '../About/About'
+import Header from '../Header/Header';
+import BuildTools from '../BuildTools/BuildTools';
+import About from '../About/About';
 import Services from '../Services/Services';
 import BrowserSupport from '../BrowserSupport/BrowserSupport';
 import HappyClient from '../HappyClient/HappyClient';
 import Pricing from '../Pricing/Pricing';
+import Reviews from '../Reviews/Reviews';
 
 const Home = () => {
     return (
@@ -17,6 +18,7 @@ const Home = () => {
             <BrowserSupport/>
             <HappyClient/>
             <Pricing/>
+            <Reviews/>
         </main>
 
     );
